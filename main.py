47cfd6da61af04094c114395ccc572cf79498a4b
Miguel William <116318328+miguelwilliam@users.noreply.github.com>
2026-02-19 17:46:32 -0300

    cospindo json

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-tweet-content = input()
+from json import loads
+
+with open('config/config.json', 'r') as f:
+    config_json_data = loads(f.read())
+
+def generateTweet():
+    tweet_content = input('Digite o Tweet: ')
+    
+    print('Escolha a conta:')
+    for user in config_json_data:
+        print(f'{user} - {config_json_data[str(user)]["name"]}')
+    user = input()
+
+
+    tweet_json = f'{{"{int(sorted(config_json_data.keys())[-1])+1}": {{"content": "{tweet_content}", "user": "{user}"}} }}'
+    return tweet_json
+
+def saveTweet(tweet_json):
+    pass
+
+my_tweet = generateTweet()
+print(my_tweet)
